5c05b6a5c3ca0c14baa1959c4913a74a1dcc9095
Anup490 <anupbhandari490@gmail.com>
2020-12-22 09:42:15 +0545

    Working with camera movement

--- Source/MuffinWar/Private/MuffinWarCharacter.cpp
+++ Source/MuffinWar/Private/MuffinWarCharacter.cpp
@@ -76,8 +76,9 @@ void AMuffinWarCharacter::SetupPlayerInputComponent(class UInputComponent* Playe
 	// We have 2 versions of the rotation bindings to handle different kinds of devices differently
 	// "turn" handles devices that provide an absolute delta, such as a mouse.
 	// "turnrate" is for devices that we choose to treat as a rate of change, such as an analog joystick
-	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
-	PlayerInputComponent->BindAxis("TurnRate", this, &AMuffinWarCharacter::TurnAtRate);
+	PlayerInputComponent->BindAxis("Turn", this, &AMuffinWarCharacter::TurnAtRate);
+	//PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+	//PlayerInputComponent->BindAxis("TurnRate", this, &AMuffinWarCharacter::TurnAtRate);
 	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
 	PlayerInputComponent->BindAxis("LookUpRate", this, &AMuffinWarCharacter::LookUpAtRate);
 
